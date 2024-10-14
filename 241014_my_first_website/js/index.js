3c01bc7b1a025f29a9160f95eb3c14fb275c6784
松懈 <58425836+oaomumu@users.noreply.github.com>
2024-10-14 22:31:21 +0800

    add 241014_my_first_website

---
+++ 241014_my_first_website/js/index.js
@@ -0,0 +1,7 @@
+fetch('IMG_sognxie.txt')
+    .then(response => response.text())
+    .then(data => {
+        const imgElement = document.getElementById('myImage');
+        imgElement.src = data; // 设置图片的 src 属性
+    })
+    .catch(error => console.error('Error loading image:', error));
