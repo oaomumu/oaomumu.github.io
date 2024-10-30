c265a286afb85ef5f971620f5020f05ed1c02ff4
松懈 <58425836+oaomumu@users.noreply.github.com>
2024-10-30 20:53:46 +0800

    add 241028_TFB_HTML&CSS

---
+++ 241028_TFB_HTML&CSS/重命名操作（failed）/重命名.bat
@@ -0,0 +1,29 @@
+REN 001-使用 CSS 的简单视差滚动效果 001 - Simple Parallax Scrolling Effect
+REN 002-使用 Html 和 CSS 的全屏视频背景 002 - Fullscreen Video Background with HTML and CSS
+REN 003-CSS3 滚动转换效果 003 - CSS3 Scroll Transition Effect
+REN 004-全屏切换菜单 004 - Fullscreen Toggle Menu
+REN 005-Box Shadow加载动画效果 005 - Box Shadow Loading Animation
+REN 006-CSS反向悬停效果 006 - CSS Reverse Hover Effect
+REN 007-垂直和水平对齐文本中心 007 - Vertically and Horizontally Centered Text
+REN 008-CSS悬停效果之字形D 008 - CSS Hover Effect Zigzag D
+REN 009-创建CSS图标悬停效果 009 - Create CSS Icon Hover Effect
+REN 010-CSS透明文本效果 010 - CSS Transparent Text Effect
+REN 011-悬停分割图像 011 - Hover Split Image
+REN 012-纯CSS霓虹灯文本效果 012 - Pure CSS Neon Text Effect
+REN 013-登录表单 013 - Login Form
+REN 014-透明的登录表单 014 - Transparent Login Form
+REN 015-Css3背景脉冲动画 015 - CSS3 Background Pulse Animation
+REN 016-图像缩放效果 016 - Image Scaling Effect
+REN 017-灰度悬停效果 017 - Grayscale Hover Effect
+REN 018-改变文本选择颜色 018 - Change Text Selection Color
+REN 019-菜单悬停效果 019 - Menu Hover Effect
+REN 020-图标滑动悬停效果 020 - Icon Slide Hover Effect
+REN 021-非常简单的CSS加载动画 021 - Very Simple CSS Loading Animation
+REN 024-使用CSS创建谷歌logo 024 - Create Google Logo with CSS
+REN 026-CSS渐变效果 026 - CSS Gradient Effect
+REN 027-css3 div悬停效果 027 - CSS3 Div Hover Effect
+REN 033-滑动社交媒体图标 033 - Sliding Social Media Icons
+REN 036-css发光位置指针动画 036 - CSS Glowing Position Pointer Animation
+REN 038-css边框动画效果 038 - CSS Border Animation Effect
+REN 040-滑动汉堡菜单图标的效果 040 - Sliding Hamburger Menu Icon Effect
+REN 052-装满水的杯子 052 - Filling Water Cup
